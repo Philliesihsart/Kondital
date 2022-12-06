2d6e897c6514fb40db9d89a22fa79181af3c9175
Phillip Marek Hansen <erligeglad2@gmail.com>
2022-12-06 20:05:38 +0100

    Added Indtastninger

--- Git/Program.cs
+++ Git/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Kondital;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.Write("Indtast din max puls i heltal slag i minuttet: ");
+        int maxpuls = Convert.ToInt32(Console.ReadLine());
+        Console.Clear();
+
+        Console.Write("Indtast din hvilepuls i heltal slag i minuttet: ");
+        int hvilepuls = Convert.ToInt32(Console.ReadLine());
+        Console.Clear();
+
+        Console.Write("Indtast din vægt (kg): ");
+        double vægt = Convert.ToDouble(Console.ReadLine());
+        Console.Clear();
+
+        Console.WriteLine("maxpuls: {0}\nHvilepuls: {1}\nVægt: {2}", maxpuls,hvilepuls,vægt);
+    }
+
+}
