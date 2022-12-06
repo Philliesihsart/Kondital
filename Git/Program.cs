b4ab444d2a7eb7b340585e1b67bb86f38d5535f0
Phillip Marek Hansen <erligeglad2@gmail.com>
2022-12-06 20:08:17 +0100

    Added Kondital Class

--- Git/Program.cs
+++ Git/Program.cs
@@ -16,7 +16,12 @@ class Program
         double vægt = Convert.ToDouble(Console.ReadLine());
         Console.Clear();
 
-        Console.WriteLine("maxpuls: {0}\nHvilepuls: {1}\nVægt: {2}", maxpuls,hvilepuls,vægt);
+        Console.WriteLine("maxpuls: {0}\nHvilepuls: {1}\nVægt: {2}", maxpuls, hvilepuls, vægt);
     }
+
+}
+
+class Kondital
+{
 
 }
